acd53209bd89e83c3a69fa2f31699e1e86a0ebd8
Danny Valerio <misscoollex@gmail.com>
2015-04-18 03:42:15 -0400

    v. 0.3.7

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+    var sound;
+    
     $("#burger").click(function(e) {
         e.preventDefault();
         $("#options").toggleClass("open");
@@ -6,8 +8,38 @@ $(document).ready(function() {
         $(this).toggleClass("animated shake");
         // on click get window height
         // set menu to window height
+        sound.play();
     });
-    
+ 
+    function setup() 
+    {
+      createCanvas(200, 200);
+
+      // initialize sounds
+      soundFormats('mp3', 'ogg');
+      sound = loadSound('Woosh-Mark_DiAngelo-4778593.mp3');
+
+      // display instructions
+      textAlign(CENTER);
+      fill(100);
+      noStroke();
+      text("Click to play sound", width/2, height/2);
+    }
+
+    function draw()
+    {
+
+    }
+
+    function mousePressed() 
+    {
+      // trigger sound
+      sound.play();
+
+      // change background color
+      background(random(255), random(255), random(255));
+
+}
     $("#dim").click(function() {
         $(this).removeClass("open");
         $("#options").removeClass("open");
@@ -60,6 +92,9 @@ $(document).ready(function() {
 //      ellipse(50, 50, 80, 80);
 //    };
     //
+    
+    var userID = BasicProfile.getId();
+    console.log(userID);
 });
 //        $("#clickme").click(function()
 //            {
---
+++ oauth2callback/gsigninapi.js
@@ -0,0 +1,9 @@
+var GoogleAuth = {};
+
+gapi.auth2.init() {
+    client_id: '215711565630-0lioh4gg3quoek0vaal9p8tp74g16t12.apps.googleusercontent.com'
+    // Additional optional params
+    };
+
+gapi.auth2.getAuthInstance();
+console.log(gapi.auth2.GoogleAuth);
