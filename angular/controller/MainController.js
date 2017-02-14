2c44c4230d357cb2845b81f89ceee3834fa63d69
Danny V <Danny V>
2017-02-14 02:19:40 -0500

    rebooting v0.01

--- angular/controller/MainController.js
+++ angular/controller/MainController.js
@@ -2,8 +2,24 @@ app.controller('MainController', ['$scope', function ($scope) {
     $scope.title = 'danny valerio';
     $scope.blocks = [
         {
-            name: '',
+            name: 'block 1',
             order: 1
+        },
+        {
+            name: 'block 2',
+            order: 2
+        },
+        {
+            name: 'block 3',
+            order: 3
+        },
+        {
+            name: 'block 4',
+            order: 4
+        },
+        {
+            name: 'block 5',
+            order: 5
         }
     ];
 }]);
