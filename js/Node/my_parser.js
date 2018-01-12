463e92a475448229f3e55bc1cd41001197d247e6
Danny V <Danny V>
2018-01-12 08:09:17 -0500

    2018

--- js/Node/my_parser.js
+++ js/Node/my_parser.js
@@ -1,10 +1,10 @@
-var Parser = require('./parser');
+var Parser = require('./parser')
 
-var fs = require('fs');
+var fs = require('fs')
 
 fs.readFile('test.txt', function(err, logData) {
     if (err) throw err;
-    var text = logData.toString();
-    var parser = new Parser();
-    console.log(parser.parse(text));
-});
+    var text = logData.toString()
+    var parser = new Parser()
+    console.log(parser.parse(text))
+})
--- oauth2callback/gsigninapi.js
+++
@@ -1,9 +0,0 @@
-var GoogleAuth = {};
-
-gapi.auth2.init() {
-    client_id: '215711565630-0lioh4gg3quoek0vaal9p8tp74g16t12.apps.googleusercontent.com'
-    // Additional optional params
-    };
-
-gapi.auth2.getAuthInstance();
-console.log(gapi.auth2.GoogleAuth);
