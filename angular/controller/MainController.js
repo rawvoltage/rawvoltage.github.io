db3ad26dc393e965aa722128e84b7927e753e1b9
Danny V <Danny V>
2018-01-12 00:28:13 -0500

    2018 updates

--- angular/controller/MainController.js
+++ angular/controller/MainController.js
@@ -3,22 +3,32 @@ app.controller('MainController', ['$scope', function ($scope) {
     $scope.blocks = [
         {
             name: 'block 1',
+            img: 'block1.jpg',
+            url: 'https://google.com',
             order: 1
         },
         {
             name: 'block 2',
+            img: 'block2.jpg',
+            url: '',
             order: 2
         },
         {
             name: 'block 3',
+            img: 'block3.jpg',
+            url: '',
             order: 3
         },
         {
             name: 'block 4',
+            img: 'block4.jpg',
+            url: '',
             order: 4
         },
         {
             name: 'block 5',
+            img: 'block5.jpg',
+            url: '',
             order: 5
         }
     ];
